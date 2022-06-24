05fd468b0e921ede806d341386953a7f19360d0b
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-24 09:02:27 +0300

    ft: user updates and newsletter logic

--- artist/forms.py
+++ artist/forms.py
@@ -12,3 +12,19 @@ class PostComments(forms.ModelForm):
         model = Comments
         exclude = ['image','posted','user']
         
+class PostProfile(forms.ModelForm):
+    class Meta:
+        models = Profile
+        exclude = ['user',]
+        
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'bio']
+        
+        
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name', max_length=30)
+    email = forms.EmailField(label='Email')                                                                                    
+
+    
